fc027c93bc02b816263f61076d7998aa3b38428a
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-08 12:47:29 +0300

    head

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
       <h1>App</h1>
+      <Link to="/Page1">Page 1</Link>
+      <Link to="/Page2">Page 2</Link>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import App from './App.jsx'
+import Page1 from './pages/page1/page1.jsx'
+import Page2 from './pages/page2/page2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router basename = "/ProService">
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/Page1' element={<Page1 />} />
+        <Route path='/Page2' element={<Page2 />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
