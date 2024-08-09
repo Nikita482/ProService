79c31be1c5d61fdb06b737a52b5c04aa4c69acb6
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-09 20:10:22 +0300

    main service fixed 3

--- src/Full/Components/MainService/MainService.jsx
+++ src/Full/Components/MainService/MainService.jsx
@@ -15,7 +15,7 @@ export default function MainService(){
                     <Link to='/Page7' className={styles.Link3}><div className={styles.text}>Уход за растениями</div></Link>
                     <Link to='/Page8' className={styles.Link4}><div className={styles.text}>Установка и монтаж мебели</div></Link>
                     <Link to='/Page9' className={styles.Link5}><div className={styles.text}>Повар</div></Link>
-                    <Link to='/Page10' className={styles.Link6}><div className={styles.text}>Уход за домашними животными</div></Link>
+                    <Link to='/Page10' className={styles.Link6}><div className={styles.text}>Уход за животными</div></Link>
                 </div>
 
             </div>
