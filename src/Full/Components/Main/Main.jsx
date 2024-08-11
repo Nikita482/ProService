58e97b0bd24b1b54aa0f3fa5c6364230c09dd675
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-08-11 13:04:11 +0300

    main fixed

--- src/Full/Components/Main/Main.jsx
+++ src/Full/Components/Main/Main.jsx
@@ -1,42 +1,52 @@
-import styles from "./Main.module.css"
+import { useEffect, useRef } from 'react';
+import styles from "./Main.module.css";
+import Card from "./MainCard/MainCard";
 
-import Card from "./MainCard/MainCard"
+export default function Main() {
+    const wrapperCardRef = useRef(null);
 
+    useEffect(() => {
+        if (wrapperCardRef.current) {
+            wrapperCardRef.current.scrollLeft = 0;
+        }
+    }, []);
 
-export default function Main(){
-    return(
+    return (
         <>
             <div className={styles.wrapper}>
-                <h1 className={styles.textName}>Выберите услугу которая вам нужна</h1>
+                <h1 className={styles.textName}>Выберите услугу, которая вам нужна</h1>
 
-                <div className={styles.wrapperCard}>
+                <div 
+                    className={styles.wrapperCard}
+                    ref={wrapperCardRef}
+                >
                     <Card 
-                        to = "/Page1"
-                        name = "Прогромирование"
-                        foto = "https://i.pinimg.com/564x/d0/67/40/d06740abac6ffc8e0e5f24333ce32b8e.jpg"
+                        to="/Page1"
+                        name="Программирование"
+                        foto="https://i.pinimg.com/564x/d0/67/40/d06740abac6ffc8e0e5f24333ce32b8e.jpg"
                     />
 
                     <Card 
-                        to = "/Page2"
-                        name = "Тренер"
-                        foto = "https://i.pinimg.com/474x/ad/e9/90/ade9905c1a88a6d40b7c6564860df30f.jpg"
+                        to="/Page2"
+                        name="Тренер"
+                        foto="https://i.pinimg.com/474x/ad/e9/90/ade9905c1a88a6d40b7c6564860df30f.jpg"
                     />
 
                     <Card 
-                        to = "/Page3"
-                        name = "Фотограф"
-                        foto = "https://i.pinimg.com/564x/91/77/2e/91772e464c9c44bba97457cfa324715a.jpg"
+                        to="/Page3"
+                        name="Фотограф"
+                        foto="https://i.pinimg.com/564x/91/77/2e/91772e464c9c44bba97457cfa324715a.jpg"
                     />
 
                     <Card 
-                        to = "/Page4"
-                        name = "Курьер"
-                        foto = "https://i.pinimg.com/564x/5a/7b/9b/5a7b9bcf2117b3fe9dfa115b271cd235.jpg"
+                        to="/Page4"
+                        name="Курьер"
+                        foto="https://i.pinimg.com/564x/5a/7b/9b/5a7b9bcf2117b3fe9dfa115b271cd235.jpg"
                     />
                 </div>
 
                 <p className={styles.textFooter}>ProService соединяет покупателей и исполнителей услуг, делая этот процесс простым и удобным.</p>
             </div>
         </>
-    )
+    );
 }
